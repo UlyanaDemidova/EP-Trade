a337c674f348781c8f7fc64f8c82f689de3370a5
Ulyana <udemidova89@gmail.com>
2023-04-04 12:27:32 +0300

    task1

---
+++ TradeApp/View/AuthorizationWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TradeApp.View;
+using TradeApp.ViewModel;
+
+namespace TradeApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class AuthorizationWindow : Window
+    {
+        public AuthorizationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnEnter_Click(object sender, RoutedEventArgs e)
+        {
+            (DataContext as AuthorizationVM).Password = pbPassword.Password;
+
+            if (await (DataContext as AuthorizationVM).ValidateAuthorizationAsync())
+            {
+                new BasicWindow().Show();
+                this.Hide();
+            }
+            else
+                MessageBox.Show("Неверный логин или пароль.","Ошибка авторизации",MessageBoxButton.OK, MessageBoxImage.Stop);
+        }
+    }
+}
---
+++ TradeApp/ViewModel/AuthorizationVM.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using System.Windows;
+using TradeApp.Model.Database;
+
+namespace TradeApp.ViewModel
+{
+    public class AuthorizationVM : BaseVM
+    {
+        private string _login;
+        private string _password;
+        private string _btnContent = "Войти";
+
+        public string Login
+        {
+            get => _login;
+            set
+            {
+                _login = value;
+                OnPropertyChanged(nameof(Login));
+            }
+        }
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+
+        public string BtnContent
+        {
+            get => _btnContent;
+            set
+            {
+                _btnContent = value;
+                OnPropertyChanged(nameof(BtnContent));
+            }
+        }
+
+        public async Task<bool> ValidateAuthorizationAsync()
+        {
+            try
+            {
+                BtnContent = "Ожидайте...";
+
+                using (var context = new TradeDBEntities())
+                {
+                    var user = await context.User.FirstOrDefaultAsync(u => u.UserLogin == Login && u.UserPassword == Password);
+                    if (user != null)
+                    {
+                        return true;
+                    }
+                    else return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Stop);
+            }
+            finally
+            {
+                BtnContent = "Войти";
+            }
+            return false;
+        }
+    }
+}
